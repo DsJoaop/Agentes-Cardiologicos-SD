34bb2dc0277d57b6e6bde012590b943f60459080
William <william.debortoli@hotmail.com>
2023-11-20 19:48:35 -0300

    fix

--- MultiAgentes/AgenteServidor/src/main/java/servidor/ControllerServer.java
+++ MultiAgentes/AgenteServidor/src/main/java/servidor/ControllerServer.java
@@ -6,6 +6,8 @@ import agentes.*;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.CountDownLatch;
 import model.ControleLPA2V;
 
 public class ControllerServer{
@@ -15,6 +17,8 @@ public class ControllerServer{
         this.servidor = new AgenteServer();
     }
     
+  
+    // Modificações em ControllerServer.processar
     public List<Double> processar(DadosInterface dados) {
         List<Agente> agentes = List.of(
                 new AgObesidade(dados),
@@ -24,27 +28,27 @@ public class ControllerServer{
                 new AgTabagista(dados),
                 new AgProfissional(dados)
         );
-        
-        List<Double> entradas = new ArrayList();
-        
+
+        List<Double> entradas = new ArrayList<>();
+
         agentes.forEach(agente -> {
-            Thread thread = new Thread(() -> {
-                entradas.add(agente.avaliar());
-            });
-            thread.start();
+            entradas.add(agente.avaliar());
         });
-        Collections.shuffle(entradas);
+
         return entradas;
     }
 
-    public String processarDados(DadosInterface dados){
-        List<Double> entradasLpa2v = processar(dados);
-        String mensagem = ControleLPA2V.iniciarAlgoritmo(entradasLpa2v);
-        if (mensagem == null) {
-            return "Erro no algoritmo";
-        }
-        return mensagem;
+// Modificações em ControllerServer.processarDados
+public String processarDados(DadosInterface dados) {
+    List<Double> entradasLpa2v = processar(dados);
+    String mensagem = ControleLPA2V.iniciarAlgoritmo(entradasLpa2v);
+    System.out.println("\n\nResposta LPA2v: " + mensagem);
+    if (mensagem == null) {
+        return "Erro no algoritmo";
     }
+    return mensagem;
+}
+
     
     public static void main(String[] args) {
         ControllerServer controlador = new ControllerServer();
